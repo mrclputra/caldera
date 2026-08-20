244447028269e8e93fb3c8291e87037c17928ca3
mrclputra <marcelino.ares.putra@gmail.com>
2026-08-20 17:56:28 +0800

    compat build fixes

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 
 static void glfw_error_callback(int error, const char *description) {
    std::cout << "glfw error: " << description << "\n";
-   glfwTerminate();
 }
 static void glfw_key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
@@ -24,8 +23,8 @@ int main(int argc, char *argv[]) {
       return 1;
    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+   // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
    GLFWwindow *window;
    window = glfwCreateWindow(640, 480, "caldera", NULL, NULL);
